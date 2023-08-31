fc4bc79e63418f666679dc70795f4bf980cbac35
Javiera <javieraVM845@gmail.com>
2023-08-30 23:14:28 -0400

    Página de registro para nuevo usuario 2

--- app/src/main/java/com/javieravenegas/finanzasencasalogin/MainActivity.java
+++ app/src/main/java/com/javieravenegas/finanzasencasalogin/MainActivity.java
@@ -2,13 +2,26 @@ package com.javieravenegas.finanzasencasalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button registrarse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registrarse = (Button) findViewById(R.id.btnRegistrarse);
+
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegistrarseActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
