1180ef5cda7445b85ca961434aa953ed6103fa77
Javiera <javieraVM845@gmail.com>
2023-09-28 01:23:49 -0300

    Progreso CardView Interactivas 1 - Vistas

---
+++ app/src/main/java/com/javieravenegas/finanzasencasalogin/TusFinanzas.java
@@ -0,0 +1,14 @@
+package com.javieravenegas.finanzasencasalogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TusFinanzas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tus_finanzas);
+    }
+}
---
+++ app/src/main/java/com/javieravenegas/finanzasencasalogin/TusGastos.java
@@ -0,0 +1,14 @@
+package com.javieravenegas.finanzasencasalogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TusGastos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tus_gastos);
+    }
+}
