22605aaef623c9ca4c2a71c6a85dc6db15344ba8
Javiera <javieraVM845@gmail.com>
2023-10-23 23:27:06 -0300

    Mapa funcional

--- app/src/main/java/com/javieravenegas/finanzasencasalogin/CotizarActivity.java
+++ app/src/main/java/com/javieravenegas/finanzasencasalogin/CotizarActivity.java
@@ -3,6 +3,9 @@ package com.javieravenegas.finanzasencasalogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import org.osmdroid.views.MapView;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.config.Configuration;
 
 public class CotizarActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class CotizarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cotizar);
+
+        // Inicializa la configuración de OSM
+        Configuration.getInstance().load(getApplicationContext(), getPreferences(MODE_PRIVATE));
+
+        // Inicializa el mapa
+        MapView map = findViewById(R.id.mapview);
+        map.setMultiTouchControls(true);
+
+        // Establece la ubicación inicial (por ejemplo, Nueva York)
+        GeoPoint startPoint = new GeoPoint(-33.4489, -70.6693);
+        map.getController().setCenter(startPoint);
+        map.getController().setZoom(12);
     }
 }
