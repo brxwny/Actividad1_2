cf00746dc530fcaab25d0b1d1c3301639ee4236d
Javiera <javieraVM845@gmail.com>
2023-10-25 00:39:17 -0300

    Mapa funcional con sensor GPS de ubicación actual

--- app/src/main/java/com/javieravenegas/finanzasencasalogin/CotizarActivity.java
+++ app/src/main/java/com/javieravenegas/finanzasencasalogin/CotizarActivity.java
@@ -2,6 +2,22 @@ package com.javieravenegas.finanzasencasalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//Nuevos
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.annotation.NonNull;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Looper;
+import org.osmdroid.api.IGeoPoint;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.OverlayItem;
+//Fin nuevos
+
 import android.os.Bundle;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
@@ -9,21 +25,58 @@ import org.osmdroid.config.Configuration;
 
 public class CotizarActivity extends AppCompatActivity {
 
+    private MapView map;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private Marker marker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cotizar);
-
-        // Inicializa la configuración de OSM
         Configuration.getInstance().load(getApplicationContext(), getPreferences(MODE_PRIVATE));
-
-        // Inicializa el mapa
         MapView map = findViewById(R.id.mapview);
         map.setMultiTouchControls(true);
 
-        // Establece la ubicación inicial (por ejemplo, Nueva York)
-        GeoPoint startPoint = new GeoPoint(-33.4489, -70.6693);
-        map.getController().setCenter(startPoint);
-        map.getController().setZoom(12);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    GeoPoint currentLocation = new GeoPoint(latitude, longitude);
+
+                    if (marker != null) {
+                        marker.setPosition(currentLocation);
+                    } else {
+                        marker = new Marker(map);
+                        marker.setPosition(currentLocation);
+                        marker.setIcon(ContextCompat.getDrawable(CotizarActivity.this, R.drawable.yo_gps_icon));
+                        map.getOverlays().add(marker);
+                    }
+
+                    map.getController().setCenter(currentLocation);
+                    map.getController().setZoom(16);
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                }
+            };
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
     }
 }
