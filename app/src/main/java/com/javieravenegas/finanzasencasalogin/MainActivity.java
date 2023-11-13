96b7d25b4b6d739bbecd590e94eacb2ca4581162
Javiera <javieraVM845@gmail.com>
2023-11-13 01:52:51 -0300

    Firebase 3

--- app/src/main/java/com/javieravenegas/finanzasencasalogin/MainActivity.java
+++ app/src/main/java/com/javieravenegas/finanzasencasalogin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.javieravenegas.finanzasencasalogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,10 +10,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.javieravenegas.finanzasencasalogin.models.Usuario;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText correo, pass;
     private Button registrarse, iniciar;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         iniciar = (Button)findViewById(R.id.btnLogin);
         correo = (EditText)findViewById(R.id.txtCorreo);
         pass = findViewById(R.id.txtPassword);
+        inicializarFirebase();
 
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,13 +50,46 @@ public class MainActivity extends AppCompatActivity {
                     if(correo.getText().toString().equals("")||pass.getText().toString().equals("")){
                         Toast.makeText(MainActivity.this, "Ingrese sus credenciales", Toast.LENGTH_LONG).show();
                     }else{
-                        Intent in = new Intent(MainActivity.this, InicioActivity.class);
-                        in.putExtra("correo", correo.getText().toString());
-                        startActivity(in);
+                        verificarCredenciales(correo.getText().toString(), pass.getText().toString());
                     }
                 }catch (Exception e){
                     Toast.makeText(MainActivity.this, "Algo salió mal", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    private void verificarCredenciales(final String correo, final String pass) {
+        databaseReference.child("Usuario").orderByChild("correo").equalTo(correo).addListenerForSingleValueEvent(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Usuario usuario = snapshot.getValue(Usuario.class);
+                        if (usuario != null && usuario.getPass().equals(pass)) {
+                            Toast.makeText(MainActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(MainActivity.this, InicioActivity.class);
+                            intent.putExtra("correo", correo);
+                            startActivity(intent);
+                            return;
+                        }
+                    }
+                    Toast.makeText(MainActivity.this, "Contraseña incorrecta", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Usuario no registrado", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Error en la base de datos", Toast.LENGTH_LONG).show();
             }
         });
     }
