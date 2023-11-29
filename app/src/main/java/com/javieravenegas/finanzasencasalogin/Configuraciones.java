f2173c5389ca21c02da094b68d112bfc63e9066f
Javiera <javieraVM845@gmail.com>
2023-11-29 19:02:21 -0300

    Vistas para CRUD de Usuarios

--- app/src/main/java/com/javieravenegas/finanzasencasalogin/Configuraciones.java
+++ app/src/main/java/com/javieravenegas/finanzasencasalogin/Configuraciones.java
@@ -3,12 +3,34 @@ package com.javieravenegas.finanzasencasalogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Configuraciones extends AppCompatActivity {
 
+    private Button btnSaveCambios, btnClose;
+    private String usuarioUid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuraciones);
+
+        btnSaveCambios = findViewById(R.id.btnSaveConfig);
+        btnClose = findViewById(R.id.btnCerrarSesion);
+
+        btnSaveCambios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Configuraciones.this, "Cambios guardados con éxito", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
