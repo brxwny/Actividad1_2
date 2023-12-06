aa6f386045fb12c557457fb22be321057e60db5b
Javiera <javieraVM845@gmail.com>
2023-12-06 04:31:47 -0300

    Crear registros de nuevos gastos en FB funcional

--- app/src/main/java/com/javieravenegas/finanzasencasalogin/AddGasto.java
+++ app/src/main/java/com/javieravenegas/finanzasencasalogin/AddGasto.java
@@ -1,5 +1,6 @@
 package com.javieravenegas.finanzasencasalogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -10,21 +11,33 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.javieravenegas.finanzasencasalogin.models.Categoria;
+import com.javieravenegas.finanzasencasalogin.models.Gasto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class AddGasto extends AppCompatActivity {
     private Button finalizar;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
-    private String uiduser;
+    private String uiduser, uid, uidcat;
+    private EditText nombreGas, desGast, montoGas;
+    private long fechaGast;
 
     private Spinner spinner;
-    private String[] paths = {"Categorías","Libros"};
+    private List<String> paths = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,41 +47,45 @@ public class AddGasto extends AppCompatActivity {
         capturarId();
         inicializarFirebase();
 
+        nombreGas = findViewById(R.id.txtAddNomGast);
+        desGast = findViewById(R.id.txtAddDesGast);
+        montoGas = findViewById(R.id.intMontoAddGast);
         finalizar = findViewById(R.id.btn_FinalizarAddGast);
         spinner = findViewById(R.id.spinnerCatGast);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddGasto.this, R.layout.spinner_personalizado,paths);
+        uid = UUID.randomUUID().toString();
+        fechaGast = System.currentTimeMillis();
+        cargarCat();
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View v, int position, long id) {
-                switch (position) {
-                    case 0:
-                        Toast.makeText(AddGasto.this, "Ninguna elegida", Toast.LENGTH_LONG).show();
-                        break;
-                    case 1:
-                        Toast.makeText(AddGasto.this, "Cat 1 elegida", Toast.LENGTH_LONG).show();
-                        break;
-                    case 2:
-                        Toast.makeText(AddGasto.this, "Cat 2 elegida", Toast.LENGTH_LONG).show();
-                        break;
-                    case 3:
-                        Toast.makeText(AddGasto.this, "Cat 3 elegida", Toast.LENGTH_LONG).show();
-                        break;
+                String selectedCategory = paths.get(position);
+                if (!selectedCategory.equals("Categoría")) {
+                    obtenerUidCategoria(selectedCategory);
+                }else{
+                    uidcat = "";
                 }
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
                 Toast.makeText(AddGasto.this, "Error al seleccionar categoría", Toast.LENGTH_LONG).show();
             }
         });
-
         finalizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(AddGasto.this, TusGastos.class);
-                startActivity(i);
+                if(nombreGas.getText().toString().equals("")||desGast.getText().toString().equals("")||montoGas.getText().toString().equals("")){
+                    Toast.makeText(AddGasto.this, "Debe completar los campos obligatorios", Toast.LENGTH_LONG).show();
+                }else{
+                    try {
+                        crearNuevoRegistro();
+                        Intent i = new Intent(AddGasto.this, TusGastos.class);
+                        startActivity(i);
+                    }catch (Exception e){
+                        Toast.makeText(AddGasto.this, "Error al añadir gasto", Toast.LENGTH_LONG).show();
+                    }
+                }
             }
         });
     }
@@ -82,5 +99,71 @@ public class AddGasto extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
+    }
+
+    private void cargarCat(){
+        databaseReference.child("Categoria").orderByChild("uiduser").equalTo(uiduser).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    paths.clear();
+                    paths.add("Categoría");
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Categoria categoria = snapshot.getValue(Categoria.class);
+                        if (categoria != null) {
+                            String nombreCategoria = categoria.getNombre();
+                            paths.add(nombreCategoria);
+                        }
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(AddGasto.this, R.layout.spinner_personalizado, paths);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner.setAdapter(adapter);
+                } else {
+                    Toast.makeText(AddGasto.this, "No se encontraron categorías", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(AddGasto.this, "Error en la consulta a la base de datos", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void obtenerUidCategoria(final String nombreCategoria){
+        databaseReference.child("Categoria").orderByChild("nombre").equalTo(nombreCategoria).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Categoria categoria = snapshot.getValue(Categoria.class);
+                        if (categoria != null && categoria.getUiduser().equals(uiduser)) {
+                            uidcat = categoria.getUid();
+                            Toast.makeText(AddGasto.this, "Seleccionaste: " + nombreCategoria + " con uid: " + uidcat, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                } else {
+                    Toast.makeText(AddGasto.this, "No se encontró la categoría", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(AddGasto.this, "Error en la consulta a la base de datos", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void crearNuevoRegistro(){
+        Gasto g = new Gasto();
+        g.setUid(uid);
+        g.setUiduser(uiduser);
+        g.setFecha(fechaGast);
+        g.setNombre(nombreGas.getText().toString());
+        g.setDescripcion(desGast.getText().toString());
+        g.setMonto(montoGas.getText().toString().trim());
+        g.setUidcat(uidcat);
+
+        databaseReference.child("Gastos").child(g.getUid()).setValue(g);
     }
 }
